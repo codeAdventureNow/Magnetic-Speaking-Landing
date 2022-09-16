636d7e294a46bbcb9b479a3ba5ba068f2ddad118
[David <[nmueller855@gmail.com]>
2022-09-16 04:28:54 -0700

    swiper working but showing on screen past width of 1100px, the cards a jumpbled too close on smaller screens

--- script.js
+++ script.js
@@ -549,3 +549,37 @@ window._load_script = function (url, callback) {
   };
   addEvent(form_to_submit, "submit", form_submit);
 })();
+
+// Swiper
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  slidesPerGroup: 1,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+
+    520: {
+      slidesPerView: 2,
+    },
+
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
